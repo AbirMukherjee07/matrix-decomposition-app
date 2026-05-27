34a8cd83c1c669fbd86121aa80c570a783cdb6d2
Abir Mukherjee <abir06nit@gmail.com>
2026-05-27 16:45:52 +0530

    Updated PCA implementation and bug fixes

--- decomposition/pca.py
+++ decomposition/pca.py
@@ -1,11 +1,14 @@
 import numpy as np
-from sklearn.decomposition import PCA
 def PCA_decom(A):
-    temp = np.array(A, dtype=float)
-    pca = PCA()
-    transformed = pca.fit_transform(temp)
-    return (
-        pca.components_,
-        pca.explained_variance_,
-        transformed
-    )
+    temp = np.array(A)
+    mean = np.mean(temp, axis=0)
+    temp = temp - mean
+    cov = np.cov(temp.T)
+    w,v = np.linalg.eig(cov)
+    idx = np.argsort(w)[::-1]
+    v1 = v[:, idx[0]]
+    v2 = v[:, idx[1]]
+    u1 = v1/np.sqrt(v1.dot(v1))
+    u2 = v2 - (v2.dot(u1))*u1
+    u2 = u2/np.sqrt(u2.dot(u2))
+    return (u1, u2)
